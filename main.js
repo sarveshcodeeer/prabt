7baff5b70a5e6e20121fb441204b9fbddf4cb96e
sarveshcodeeer <116813023+sarveshcodeeer@users.noreply.github.com>
2024-04-23 20:28:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const switchButton = document.getElementById('switchButton');
+
+// Add a click event listener to the button
+switchButton.addEventListener('click', function() {
+    // Redirect to another HTML page when the button is clicked
+    window.location.href = 'another_page.html';
+});
